97e84b211b17d544fa849d868d76a063f284a072
pule <pmolefe25@gmail.com>
2025-03-21 05:59:30 -0700

    Creating database

--- models/create_db.py
+++ models/create_db.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../recipecloud.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 class Recipe(db.Model):
-    __tablename__ = 'Recipes'
+    __tablename__ = 'recipes'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=False)
     category = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
 
+
 class Ingredients(db.Model):
-    __tablename__ = 'Ingredients'
+    __tablename__ = 'ingredients'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
 
+
 class Rating(db.Model):
-     __tablename__ = 'ratings'
-     id = db.Column(db.Integer, primary_key=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+    __tablename__ = 'ratings'
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'),
+                          nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
+
 
 class Favourite(db.Model):
     __tablename__ = 'favorites'
     id = db.Column(db.Integer, primary_key=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'),
+                          nullable=False)
     user_identifier = db.Column(db.String(100), nullable=False)
+
+
+# Create tables
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        print("Tables created successfully!")
